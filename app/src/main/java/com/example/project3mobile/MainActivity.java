19d4d97f7157e9ff2049e625c3a03115b6997fec
MarcosOS98 <orozco.marcos11@gmail.com>
2022-04-12 12:28:49 -0700

    Initial Starting Project

---
+++ app/src/main/java/com/example/project3mobile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.project3mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
