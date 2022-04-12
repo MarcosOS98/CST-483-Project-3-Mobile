dd6ccb929792a9a41b82329043e921c1a51047d8
Marcos Orozco Silva <72904602+MarcosOS98@users.noreply.github.com>
2022-04-12 12:45:30 -0700

    Add files via upload

---
+++ Project3Mobile/app/src/main/java/com/example/project3mobile/AppStorage/UserDAO.java
@@ -0,0 +1,36 @@
+package com.example.project3mobile.AppStorage;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.project3mobile.User;
+
+import java.util.List;
+
+@Dao
+public interface UserDAO {
+    // Standard edits for Accounts: Insert/Update/Delete
+    @Insert
+    void insert(User... users);
+
+    @Update
+    void update(User... users);
+
+    @Delete
+    void delete(User users);
+
+    //Sorts All Users
+    @Query("SELECT * FROM " + AppDatabase.USER_TABLE)
+    List<User> getUsers();
+
+    // Sorts Users by Id
+    @Query("SELECT * FROM " + AppDatabase.USER_TABLE + " WHERE mUserId = :UserID")
+    User getUsersByID(int UserID);
+
+    //Sorts Users by Username
+    @Query("SELECT * FROM " + AppDatabase.USER_TABLE + " WHERE mUserName = :username")
+    User getUserByUsername(String username);
+}
