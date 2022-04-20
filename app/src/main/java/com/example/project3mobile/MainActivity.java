4483a01e2204ddb253139b7afd12978be2cfd6a9
Bujangja <mulmlprpg@gmail.com>
2022-04-20 13:57:26 -0700

    Added nav_graph.xml and added the navfragment to the activity_main.xml

--- app/src/main/java/com/example/project3mobile/MainActivity.java
+++ app/src/main/java/com/example/project3mobile/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.project3mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.room.Room;
 
 import android.content.Intent;
@@ -13,7 +16,6 @@ import com.example.project3mobile.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +24,7 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(binding.getRoot());
 
-        loginBtn = view.findViewById(R.id.loginBtn);
-    }
 
-    //put the name of the fragment/class you would like to load where LoginFragment.class
-    //and change name of the class name to whatever the button is named instead of login
-    public void login(View v){
-        Intent intent = new Intent(getApplicationContext(), LoginFragment.class);
-        startActivity(intent);
     }
 
 }
