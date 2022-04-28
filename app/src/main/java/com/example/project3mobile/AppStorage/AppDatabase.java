1e8a347e033885b47c85749043f65e2a054ec254
MarcosOS98 <orozco.marcos11@gmail.com>
2022-04-28 15:05:48 -0700

    Implemented some of the methods for the Login Fragment and Register Fragment

--- app/src/main/java/com/example/project3mobile/AppStorage/AppDatabase.java
+++ app/src/main/java/com/example/project3mobile/AppStorage/AppDatabase.java
@@ -1,10 +1,23 @@
 package com.example.project3mobile.AppStorage;
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.example.project3mobile.User;
-@Database(entities = {User.class}, version = 2)
+@Database(entities = {User.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase appInstance;
+    public static synchronized AppDatabase getInstance(Context context) {
+        if(appInstance == null) {
+            appInstance = Room.databaseBuilder(context.getApplicationContext(),
+                    AppDatabase.class, "")
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return appInstance;
+    }
     public static final String USER_TABLE = "USER_TABLE";
 
     public abstract UserDAO getUserDAO();
