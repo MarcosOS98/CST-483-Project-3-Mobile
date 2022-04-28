9a2273af450f1ece81c08badaaf4b7e0194a38a8
Andrew Liddle <aliddle98@gmail.com>
2022-04-28 01:41:27 -0700

    - finished all possible work on StudentRowAdapter (no repository to fill it with yet, next task)

--- app/src/main/java/com/example/project3mobile/adapters/StudentRowAdapter.java
+++ app/src/main/java/com/example/project3mobile/adapters/StudentRowAdapter.java
@@ -1,32 +1,56 @@
 package com.example.project3mobile.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class StudentRowAdapter extends RecyclerView.Adapter<StudentRowAdapter.Viewholder> {
+import com.example.project3mobile.R;
+import com.example.project3mobile.databinding.StudentRowItemBinding;
+
+public class StudentRowAdapter extends RecyclerView.Adapter<StudentRowAdapter.ViewHolder> {
+
+    private StudentRowItemBinding binding;
+    private Context context;
+
+    // TODO: DATA INITIALIZATION IN ARRAY/LIST WOULD GO HERE
+
     @NonNull
     @Override
-    public StudentRowAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public StudentRowAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.student_row_item,parent,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StudentRowAdapter.Viewholder holder, int position) {
-
+    public void onBindViewHolder(@NonNull StudentRowAdapter.ViewHolder holder, int position) {
+        // TODO: FILLING THE LIST WITH DATA WITH .GET'S GOES HERE
     }
 
     @Override
     public int getItemCount() {
+        // TODO: RETURN DATA ARRAY/LIST SIZE HERE
         return 0;
     }
 
-    public class Viewholder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
-        public Viewholder(@NonNull View itemView) {
+        TextView className;
+        TextView assignmentName;
+        LinearLayout linearLayout;
+
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            className = binding.className;
+            assignmentName = binding.assignName;
+            linearLayout = binding.layoutId;
         }
     }
 
